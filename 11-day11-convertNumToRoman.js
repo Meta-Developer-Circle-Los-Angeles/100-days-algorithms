6d32de6ad33ce0a4730eac7a64d0947934808d99
Juliette Tworsey <juliettetworsey@Mac.local>
2022-05-11 19:11:08 -0700

    fix typo in class convertToRoman

--- 11-day11-convertNumToRoman.js
+++ 11-day11-convertNumToRoman.js
@@ -45,7 +45,7 @@ class convertToRoman{
     convert(){
         let romanNum = [];
         let values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
-         et romanLiterals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+        let romanLiterals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
         for(let i=0; i<values.length; i++) {
             while(this.num >= values[i]) {
                 this.num -= values[i];
